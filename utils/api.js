1e6371d1575c4b3f3deb7ef9ceae57fe57dbecb1
Esther <11yekinie@gmail.com>
2023-12-11 15:53:20 +0000

    allow viewing of all articles

---
+++ utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://thread-sphere.onrender.com/api/",
+});
+
+export const getAllArticles = (author, topic, sort_by, order) => {
+  return instance
+    .get("/articles", {params: {author: author, topic: topic, sort_by: sort_by, order: order}})
+    .then(({ data }) => {
+      return data.articles;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
