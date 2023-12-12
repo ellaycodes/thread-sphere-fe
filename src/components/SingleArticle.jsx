65eeba5a7c2972298cdf9124253de7d2b5f8c015
Esther <11yekinie@gmail.com>
2023-12-12 15:56:18 +0000

    solve merge errors

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,35 +1,44 @@
 import { useEffect, useState } from "react";
 import { BodyHeader } from "./BodyHeader";
-import { getArticleById } from "../utils/api";
+import { getArticleById } from "../../utils/api";
 import { useParams } from "react-router-dom";
+import { UpVotes } from "./UpVotes";
+import { CommentSection } from "./CommentSection";
 
 export const SingleArticle = () => {
   const [article, setArticle] = useState([]);
+  const [isloading, setIsLoading] = useState(true);
 
   const { article_id } = useParams();
 
   useEffect(() => {
     getArticleById(article_id).then((article) => {
       setArticle(article);
+      setIsLoading(false);
     });
   }, []);
 
-  console.log(article);
+  if (isloading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <>
       <BodyHeader title={article.title} />
-      <p>{article.topic}</p>
-      <p>{article.created_at}</p>
-      <p>{article.votes}</p>
+      <div className="article_stats">
+        <p>{article.topic}</p>
+        <p>{article.created_at}</p>
+        <p>{article.votes}</p>
+        <UpVotes />
+      </div>
       <div className="article_body">
-        <img src={article.article_img_url} alt="" />
+      <img src={article.article_img_url} alt="" className="hero_img"/>
         <div className="article_content">
           <p>{article.author}</p>
           <p>{article.body}</p>
-          <p>Comments: {article.comment_count}</p>
         </div>
       </div>
+      <CommentSection article={article} />
     </>
   );
 };
