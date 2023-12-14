26c2a65031593dfe110cbccd5c2acf7053b271f7
Esther <11yekinie@gmail.com>
2023-12-14 17:12:52 +0000

    adds the ability to filter

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,29 +1,13 @@
 import { useEffect, useState } from "react";
 import { getAllArticles } from "../../utils/api";
 import { ArticleCard } from "./ArticleCard";
+import { Filter } from "./filter";
 
 export const ArticleList = () => {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [sort_by, setSortBy] = useState("created_at");
   const [order, setOrder] = useState("desc");
-
-  const handleSortChange = (event) => {
-    const sortOption = event.target.value;
-    switch (sortOption) {
-      case "Date":
-        setSortBy("created_at");
-        break;
-      case "Comment Count":
-        setSortBy("comment_count");
-        break;
-      case "Votes":
-        setSortBy("votes");
-        break;
-      default:
-        setSortBy("date");
-    }
-  };
 
   useEffect(() => {
     setIsLoading(true);
@@ -37,25 +21,13 @@ export const ArticleList = () => {
       });
   }, [sort_by, order]);
 
-  const toggleOrder = () => {
-    setOrder(order === "asc" ? "desc" : "asc");
-  };
-
   if (isLoading) {
     return <h2>Loading...</h2>;
   }
 
   return (
     <>
-      <select name="filter" id="filter" onChange={handleSortChange}>
-        {/* <option value="Sort By">Sort By</option> */}
-        <option value="Date">Date</option>
-        <option value="Comment Count">Comment Count</option>
-        <option value="Votes">Votes</option>
-      </select>
-      <button onClick={toggleOrder}>
-        {order === "asc" ? '⬆' : "⬇"}
-      </button>
+      <Filter setSortBy={setSortBy} order={order} setOrder={setOrder}/>
       {articles.map((article) => {
         return <ArticleCard article={article} key={article.article_id} />;
       })}
--- src/components/TopicArticles.jsx
+++ src/components/TopicArticles.jsx
@@ -3,28 +3,32 @@ import { getAllArticles } from "../../utils/api";
 import { useParams } from "react-router-dom";
 import { ArticleCard } from "./ArticleCard";
 import { BodyHeader } from "./BodyHeader";
+import { Filter } from "./filter";
 
 export const TopicArticles = () => {
   const { topic } = useParams();
   const [articles, setArticles] = useState([]);
+  const [sort_by, setSortBy] = useState('created_at')
+  const [order, setOrder] = useState("desc");
+
   useEffect(() => {
-    getAllArticles().then((articleArr) => {
+    getAllArticles(sort_by, order).then((articleArr) => {
       const filteredArticles = articleArr.filter(
         (article) => article.topic === topic
       );
       setArticles(filteredArticles);
     });
-  }, [topic]);
+  }, [topic, sort_by, order]);
 
   return (
     <>
       <BodyHeader title={topic.toUpperCase()} />
+      <Filter setSortBy={setSortBy} order={order} setOrder={setOrder}/>
       {articles.map((article) => {
         return (
-          <ArticleCard
-            article={article}
-            key={`${article.article_id}${article.topic}`}
-          />
+            <ArticleCard
+              article={article}
+              key={`${article.article_id}${article.topic}`}/>
         );
       })}
     </>
